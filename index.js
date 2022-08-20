b9e03aeec242844ceeae5900de997907b1358b5b
Kayla Newlon <kayla.b.newlon@gmail.com>
2022-08-19 22:04:05 -0700

    set up express

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const pa11y = require('pa11y')
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+app.get('/api/test', (req,res) => {
+    if(!req.query.url){
+        res.status(400).json({error: "url is required"})
+    }
+})
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
